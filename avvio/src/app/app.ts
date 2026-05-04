e060d42c85f40cc42be70a738b38597478c8c1df
mvaleei <info@marcellovaleri.it>
2026-05-04 16:44:36 +0200

    4 Pomeriggio con esercizio

--- avvio/src/app/app.ts
+++ avvio/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
+  //template: "<div>Saluti da Roma</div>",
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('avvio');
+  //protected readonly title = signal('avvio');
+  title: string = "Primo progetto in angular";
+  corso: string = "Angular Base";
+  anni: number = 15;
+
+
+  cittaNascita: string = "Livorno";
+
+
+  metodo1(): void {
+    alert(`Sto provando il binding di tipo: event binding: ${this.anni}`)
+    //alert("Sto provando il binding di tipo: event binding " + this.anni);
+  }
+
 }
